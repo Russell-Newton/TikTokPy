860704f4ede9b542287095ff7b9921845a2b1ebf
Russell Newton <russell.newton01@gmail.com>
2023-01-01 15:05:59 -0500

    Rollover to 0.1.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath("../ext"))  # Custom ext dir relative to this
 project = "TikTokPy"
 copyright = "2022, Russell Newton"
 author = "Russell Newton"
-release = "0.1.6"
+release = "0.1.7"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
